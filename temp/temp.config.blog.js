74a755b3e7b0d8a2a15714e90efbbf6a096e45ea
StayAndGo <wthuck@163.com>
2022-09-04 20:34:51 +0800

    Revert "fix: 暂存"
    This reverts commit 3830c049d5a106223c12d7969b2abfb994b4ecbe.

--- temp/temp.config.blog.js
+++ temp/temp.config.blog.js
@@ -2,18 +2,13 @@ const controller = require('../service/mysql');
 
 module.exports = TEMP_BLOG_CONFIG = async siteId => {
     const [ data ] = await controller.getSiteInfo(siteId);
-    const [ COMMENT ] = await controller.getComment({
-        // for dev only
-        siteId: '8f290df2-16a4-489e-ba1b-bf57fd2f6e10'
-    });
 
     const FRIENDSHIP = await controller.getFriendshipList();
     return new Promise((resolve, reject) => {
         resolve(`module.exports = {
     title: '${data.name}',
     logo: '${data.logo}',
-    friendship: ${JSON.stringify(FRIENDSHIP)},
-    comment: ${JSON.stringify(COMMENT)}
+    friendship: ${JSON.stringify(FRIENDSHIP)}
 }`
 )
     })
